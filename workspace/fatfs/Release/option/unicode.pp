4c44724250a4683653b8d619414ad1a1262866b4
santa_ryan <ryan.w.claus@gmail.com>
2015-01-27 01:25:14 -0700

    "driverlib", "grlib", and "usblib" Debug and Release makefile updated by CCS6
    Added FatFS library for TM4C123G
    
    Signed-off-by: santa_ryan <ryan.w.claus@gmail.com>

---
+++ workspace/fatfs/Release/option/unicode.pp
@@ -0,0 +1,11 @@
+# FIXED
+
+option/unicode.obj: ../option/unicode.c
+option/unicode.obj: ../option/../ff.h
+option/unicode.obj: ../option/../integer.h
+option/unicode.obj: ../option/../ffconf.h
+
+../option/unicode.c: 
+../option/../ff.h: 
+../option/../integer.h: 
+../option/../ffconf.h: 

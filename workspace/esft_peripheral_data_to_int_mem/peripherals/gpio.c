d2dc58ff400230718deafd0a422ff37eb7bc6ec0
Ryan William Claus <ryan.w.claus@gmail.com>
2015-02-23 01:18:13 -0700

    GPIO raw input/output functions added for future use with external interrupts and chute deployment
    Signed-off-by: Ryan William Claus <ryan.w.claus@gmail.com>

---
+++ workspace/esft_peripheral_data_to_int_mem/peripherals/gpio.c
@@ -0,0 +1,33 @@
+/*
+ * gpio.c
+ *
+ *  Created on: Feb 22, 2015
+ *      Author: Ryan
+ */
+
+
+#include "driverlib/gpio.h"
+#include "driverlib/rom.h"
+#include "driverlib/rom_map.h"
+#include "inc/hw_gpio.h"
+
+#include "gpio.h"
+
+void
+gpioInitInput(uint32_t ui32Port, uint8_t ui8Pins, uint32_t ui32Strength, uint32_t ui32PinType) {
+  MAP_SysCtlPeripheralEnable(ui32Port);
+  MAP_GPIOPinTypeGPIOInput(ui32Port, ui8Pins);
+  MAP_GPIOPadConfigSet(ui32Port, ui8Pins, ui32Strength, ui32PinType);
+}
+void
+gpioInitOutput(uint32_t ui32Port, uint8_t ui8Pins, uint32_t ui32Strength, uint32_t ui32PinType) {
+  MAP_SysCtlPeripheralEnable(ui32Port);
+  MAP_GPIOPinTypeGPIOOutput(ui32Port, ui8Pins);
+  MAP_GPIOPadConfigSet(ui32Port, ui8Pins, ui32Strength, ui32PinType);
+}
+void
+gpioInitOutputOD(uint32_t ui32Port, uint8_t ui8Pins, uint32_t ui32Strength, uint32_t ui32PinType) {
+  MAP_SysCtlPeripheralEnable(ui32Port);
+  MAP_GPIOPinTypeGPIOOutputOD(ui32Port, ui8Pins);
+  MAP_GPIOPadConfigSet(ui32Port, ui8Pins, ui32Strength, ui32PinType);
+}
